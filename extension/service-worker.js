f60be010814b2b9da1fc530273e24be1571df1fa
nownabe <nownabe@gmail.com>
2023-07-30 17:02:53 +0900

    Add Chrome Extension for PoC

---
+++ extension/service-worker.js
@@ -0,0 +1,27 @@
+const host = "your-host";
+
+async function initialize() {
+  const redirectRule = {
+    id: 1,
+    priority: 1,
+    action: {
+      type: "redirect",
+      redirect: {
+        transform: { scheme: "https", host: host },
+      },
+    },
+    condition: {
+      urlFilter: "*://go/*",
+      resourceTypes: ["main_frame"],
+    },
+  };
+
+  const updateRuleOptions = {
+    removeRuleIds: [1],
+    addRules: [redirectRule],
+  };
+
+  await chrome.declarativeNetRequest.updateDynamicRules(updateRuleOptions);
+}
+
+chrome.runtime.onInstalled.addListener(initialize);
