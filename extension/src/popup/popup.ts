af35ea581ff3ac6a95082f6122d6badce8b8a4ec
nownabe <nownabe@gmail.com>
2023-08-01 01:08:44 +0900

    Use baseUrl from storage

--- extension/src/popup/popup.ts
+++ extension/src/popup/popup.ts
@@ -2,37 +2,59 @@ import { MethodKind } from "@bufbuild/protobuf";
 import { createPromiseClient } from "@bufbuild/connect";
 import { createConnectTransport } from "@bufbuild/connect-web";
 
+import type { PromiseClient } from "@bufbuild/connect";
+
 import {
   CreateGolinkRequest,
   CreateGolinkResponse,
 } from "../../gen/golink/v1/golink_pb";
 import { GolinkService } from "../../gen/golink/v1/golink_connect";
 
-const apiBaseUrl = "http://localhost:8081/api/";
+const golinkUrlKey = "golinkUrl";
 
 class GolinkPopup {
-  // TODO: Don't use any
-  constructor(client: any) {
-    self.client = client;
+  client: PromiseClient<GolinkService>;
+
+  constructor(client: PromiseClient<GolinkService>) {
+    this.client = client;
   }
 
-  async onSave() {
-    const res = await client.createGolink({
+  setClient(client: PromiseClient<GolinkService>) {
+    this.client = client;
+  }
+
+  onSave = async () => {
+    console.log(this.client);
+    const res = await this.client.createGolink({
       name: "mylink",
       url: "https://example.com",
     });
     console.log(res);
-  }
+  };
+}
+
+async function buildClient() {
+  const url = (await chrome.storage.sync.get(golinkUrlKey))[golinkUrlKey];
+  const transport = createConnectTransport({
+    baseUrl: url + "/api",
+    credentials: "include",
+  });
+  return createPromiseClient(GolinkService, transport);
 }
 
 async function initialize() {
-  const transport = createConnectTransport({
-    baseUrl: apiBaseUrl,
-    credentials: "include",
-  });
-  const client = createPromiseClient(GolinkService, transport);
+  const popup = new GolinkPopup(await buildClient());
 
-  const popup = new GolinkPopup(client);
+  chrome.storage.onChanged.addListener(
+    async (
+      changes: { [key: string]: chrome.storage.StorageChange },
+      namespace: string,
+    ) => {
+      if (namespace === "sync" && golinkUrlKey in changes) {
+        popup.setClient(await this.buildClient());
+      }
+    },
+  );
 
   document.getElementById("save").addEventListener("click", popup.onSave);
   document.getElementById("cancel").addEventListener("click", popup.onCancel);
