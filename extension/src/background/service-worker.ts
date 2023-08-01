31f284197a9cd197b1a0a5ca5dfc275a7eb95d20
nownabe <nownabe@gmail.com>
2023-08-01 21:24:28 +0900

    Get Golink URL from storage

--- extension/src/background/service-worker.ts
+++ extension/src/background/service-worker.ts
@@ -1,8 +1,13 @@
-const host = "your-host.com";
+const golinkUrlKey = "golinkUrl";
 
-async function initialize() {
+async function updateRedirectRule(url: string) {
+  const ruleId = 1;
+
+  const host = new URL(url).host;
+  console.log(`Updating redirect rule to ${host}`);
+
   const redirectRule = {
-    id: 1,
+    id: ruleId,
     priority: 1,
     action: {
       type: "redirect",
@@ -17,11 +22,28 @@ async function initialize() {
   };
 
   const updateRuleOptions = {
-    removeRuleIds: [1],
+    removeRuleIds: [ruleId],
     addRules: [redirectRule],
   };
 
   await chrome.declarativeNetRequest.updateDynamicRules(updateRuleOptions);
+}
+
+async function initialize() {
+  const url = (await chrome.storage.sync.get(golinkUrlKey))[golinkUrlKey];
+
+  await updateRedirectRule(url);
+
+  chrome.storage.onChanged.addListener(
+    async (
+      changes: { [key: string]: chrome.storage.StorageChange },
+      namespace: string,
+    ) => {
+      if (namespace === "sync" && golinkUrlKey in changes) {
+        await updateRedirectRule(changes[golinkUrlKey].newValue);
+      }
+    },
+  );
 }
 
 chrome.runtime.onInstalled.addListener(initialize);
