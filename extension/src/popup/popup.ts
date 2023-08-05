f1e88f7cb48df1e7fe45789859f6aa5b46a53d16
nownabe <nownabe@gmail.com>
2023-08-05 10:46:09 +0900

    Popup's authentication flow

--- extension/src/popup/popup.ts
+++ extension/src/popup/popup.ts
@@ -15,51 +15,130 @@ const golinkUrlKey = "golinkUrl";
 class GolinkPopup {
   client: PromiseClient<typeof GolinkService>;
 
-  constructor(client: PromiseClient<typeof GolinkService>) {
-    this.client = client;
+  constructor(url: string) {
+    this.setUrl(url);
   }
 
-  setClient(client: PromiseClient<typeof GolinkService>) {
-    this.client = client;
+  setUrl(url: string) {
+    this.url = url;
+    this.api = url + "api";
+
+    const transport = createConnectTransport({
+      baseUrl: this.api,
+      credentials: "include",
+    });
+    this.client = createPromiseClient(GolinkService, transport);
+  }
+
+  async checkAuth(): boolean {
+    try {
+      const res = await fetch(this.api + "/healthz", {
+        credentials: "include",
+      });
+      return res.status === 200;
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
   }
 
   onSave = async () => {
-    console.log(this.client);
     const res = await this.client.createGolink({
       name: "mylink",
       url: "https://example.com",
     });
     console.log(res);
   };
+
+  openConsole = async () => {
+    chrome.tabs.create({ url: this.url + "c" });
+  };
 }
 
-async function buildClient() {
+async function getGolinkUrl(): Promise<string | null> {
   let url = (await chrome.storage.sync.get(golinkUrlKey))[golinkUrlKey];
+  if (!url) {
+    return null;
+  }
+
   if (!url.endsWith("/")) {
     url += "/";
   }
 
-  const transport = createConnectTransport({
-    baseUrl: url + "api",
-    credentials: "include",
-  });
-  return createPromiseClient(GolinkService, transport);
+  return url;
+}
+
+function updateClientFunc(popup: GolinkPopup) {
+  const notConfiguredElem = document.getElementById("not-configured");
+  const unauthenticatedElem = document.getElementById("unauthenticated");
+  const golinkUiElem = document.getElementById("golink-ui");
+
+  const showNotConfigured = () => {
+    notConfiguredElem.hidden = false;
+    unauthenticatedElem.hidden = true;
+    golinkUiElem.hidden = true;
+  };
+
+  const showUnauthenticated = () => {
+    notConfiguredElem.hidden = true;
+    unauthenticatedElem.hidden = false;
+    golinkUiElem.hidden = true;
+  };
+
+  const showGolinkUi = () => {
+    notConfiguredElem.hidden = true;
+    unauthenticatedElem.hidden = true;
+    golinkUiElem.hidden = false;
+  };
+
+  return async () => {
+    const url = await getGolinkUrl();
+
+    if (!url) {
+      showNotConfigured();
+      return;
+    }
+
+    popup.setUrl(url);
+
+    if (await popup.checkAuth()) {
+      showGolinkUi();
+    } else {
+      showUnauthenticated();
+    }
+  };
+}
+
+function urlCallback(updateClient: () => Promise<void>) {
+  return async (
+    changes: { [key: string]: chrome.storage.StorageChange },
+    namespace: string,
+  ) => {
+    if (namespace === "sync" && golinkUrlKey in changes) {
+      console.log("url changed");
+      await updateClient();
+    }
+  };
+}
+
+async function openOptionsPage() {
+  await chrome.runtime.openOptionsPage();
 }
 
 async function initialize() {
-  const popup = new GolinkPopup(await buildClient());
+  const popup = new GolinkPopup();
 
-  chrome.storage.onChanged.addListener(
-    async (
-      changes: { [key: string]: chrome.storage.StorageChange },
-      namespace: string,
-    ) => {
-      if (namespace === "sync" && golinkUrlKey in changes) {
-        popup.setClient(await this.buildClient());
-      }
-    },
-  );
+  const updateClient = updateClientFunc(popup);
+  await updateClient();
 
+  chrome.storage.onChanged.addListener(urlCallback(updateClient));
+
+  document
+    .getElementById("open-options")
+    .addEventListener("click", openOptionsPage);
+  document
+    .getElementById("open-console")
+    .addEventListener("click", popup.openConsole);
   document.getElementById("save").addEventListener("click", popup.onSave);
   document.getElementById("cancel").addEventListener("click", popup.onCancel);
   console.log("initialized");
