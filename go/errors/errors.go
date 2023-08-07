f4c3a50c5ae5d6fe4350fcf3bb3a77a719b4d65e
nownabe <nownabe@gmail.com>
2023-08-08 00:19:03 +0900

    Update errors

--- go/errors/errors.go
+++ go/errors/errors.go
@@ -1,6 +1,7 @@
 package errors
 
 import (
+	"fmt"
 	"runtime/debug"
 
 	"github.com/nownabe/golink/go/clog"
@@ -11,6 +12,14 @@ func New(msg string) error {
 		err:   nil,
 		msg:   msg,
 		stack: debug.Stack(),
+	}
+}
+
+func NewWithoutStack(msg string) error {
+	return &wrapped{
+		err:   nil,
+		msg:   msg,
+		stack: nil,
 	}
 }
 
@@ -29,6 +38,32 @@ func Wrap(err error, msg string) error {
 		err:   err,
 		msg:   msg,
 		stack: debug.Stack(),
+	}
+}
+
+func Wrapf(err error, format string, args ...any) error {
+	return Wrap(err, fmt.Sprintf(format, args...))
+}
+
+func Is(err error, target error) bool {
+	if err == nil && target == nil {
+		return true
+	}
+
+	for {
+		if err == nil {
+			return false
+		}
+
+		if err == target {
+			return true
+		}
+
+		if w, ok := err.(*wrapped); ok {
+			err = w.err
+		} else {
+			return false
+		}
 	}
 }
 
