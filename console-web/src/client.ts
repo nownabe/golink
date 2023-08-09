c0a0d304d04a9129ff8b0e5db99d9964f21b63dc
nownabe <nownabe@gmail.com>
2023-08-09 22:48:07 +0900

    Use same host /api if not specified

--- console-web/src/client.ts
+++ console-web/src/client.ts
@@ -3,7 +3,7 @@ import { createConnectTransport } from "@bufbuild/connect-web";
 
 import { GolinkService } from "./gen/golink/v1/golink_connect";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL || "";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || "/api";
 
 const transport = createConnectTransport({
   baseUrl: apiUrl,
