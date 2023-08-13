87ac4b03b609159bd8350cab75c7d6c504ce16df
nownabe <nownabe@gmail.com>
2023-08-13 22:41:05 +0900

    Print error in Debug endpoint

--- api/debug_service.go
+++ api/debug_service.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/nownabe/golink/go/clog"
+	"github.com/nownabe/golink/go/errors"
 
 	golinkv1 "github.com/nownabe/golink/api/gen/golink/v1"
 )
@@ -14,17 +16,26 @@ func (s *debugService) Debug(
 	ctx context.Context,
 	req *connect.Request[golinkv1.DebugRequest],
 ) (*connect.Response[golinkv1.DebugResponse], error) {
-	debug1(ctx, req)
+	if err := debug1(ctx, req); err != nil {
+		clog.Err(ctx, errors.Wrap(err, "debug1 failed"))
+	}
 	return connect.NewResponse(&golinkv1.DebugResponse{}), nil
 }
 
-func debug1(ctx context.Context, req *connect.Request[golinkv1.DebugRequest]) {
-	debug2(ctx, req)
+func debug1(ctx context.Context, req *connect.Request[golinkv1.DebugRequest]) error {
+	if err := debug2(ctx, req); err != nil {
+		return errors.Wrap(err, "debug2 failed")
+	}
+	return nil
 }
 
-func debug2(ctx context.Context, req *connect.Request[golinkv1.DebugRequest]) {
-	debug3(ctx, req)
+func debug2(ctx context.Context, req *connect.Request[golinkv1.DebugRequest]) error {
+	if err := debug3(ctx, req); err != nil {
+		return errors.Wrap(err, "debug3 failed")
+	}
+	return nil
 }
 
-func debug3(ctx context.Context, req *connect.Request[golinkv1.DebugRequest]) {
+func debug3(ctx context.Context, req *connect.Request[golinkv1.DebugRequest]) error {
+	return errors.New("debug error")
 }
