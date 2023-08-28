9a08c69d43490062d586616eac86a71ea2860d74
nownabe <nownabe@gmail.com>
2023-08-28 21:45:28 +0900

    Revert "Delete unused file"
    This reverts commit a18635deccc2e4b7365d89191652772af892824d.

---
+++ go/golinkcontext/context.go
@@ -0,0 +1,26 @@
+package golinkcontext
+
+import "context"
+
+type (
+	contextKeyUserEmail struct{}
+	contextKeyUserID    struct{}
+)
+
+func WithUserEmail(ctx context.Context, email string) context.Context {
+	return context.WithValue(ctx, contextKeyUserEmail{}, email)
+}
+
+func UserEmailFrom(ctx context.Context) (string, bool) {
+	v, ok := ctx.Value(contextKeyUserEmail{}).(string)
+	return v, ok
+}
+
+func WithUserID(ctx context.Context, id string) context.Context {
+	return context.WithValue(ctx, contextKeyUserID{}, id)
+}
+
+func UserIDFrom(ctx context.Context) (string, bool) {
+	v, ok := ctx.Value(contextKeyUserID{}).(string)
+	return v, ok
+}
